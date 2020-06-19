f52e98d8ea5ffcbc46e18673d32c270298f1402f
guisalmeida <guilherme.dasilva@linx3.com>
2020-06-19 18:56:20 -0300

    add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
+import * as S from '../components/Post/styled';
 
 const AboutPage = () => (
     <Layout>
         <SEO title="Sobre" />
-        <h1>about page</h1>
+        <S.PostHeader>
+            <S.PostTitle>Sobre mim</S.PostTitle>
+            <S.PostDescription>
+                Olá sou Guilherme Almeida, profissional criativo e inovador, atuo como designer e desenvolvedor web e procuro estar sempre atualizado na área criativa e tecnológica.
+            </S.PostDescription>
+            <S.PostDescription>
+                Buscando crescimento profissional e novos desafios, atualmente venho me dedicando em programação e desenvolvimento web front-end, visando me tornar um profissional mais capacitado para trabalhar com as demandas que o mercado oferece.
+            </S.PostDescription>
+        </S.PostHeader>
+        <S.MainContent>
+        </S.MainContent>
     </Layout>
 );
 
