121a674d4c552bdacff1038e370b5fe0a6181bf3
guisalmeida <guilherme.dasilva@linx3.com>
2020-06-29 19:25:51 -0300

    fix component render to search page

--- src/components/Search/Hit.js
+++ src/components/Search/Hit.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import PostItem from '../PostItem';
+import ProjectItem from '../ProjectItem';
 
 const Hit = ({ hit }) => {
-    return (
-        <PostItem
-            slug={hit.fields.slug}
-            title={hit.title}
-            date={hit.date}
-            description={hit.description}
-            category={hit.category}
-            thumbnailImage={hit.thumbnailImage.relativePath}
-            timeToRead={hit.timeToRead}
-        />
-    )
+    if(hit.category === 'project') {
+        return (
+            <ProjectItem
+                slug={hit.fields.slug}
+                title={hit.title}
+                description={hit.description}
+                thumbnailImage={hit.thumbnailImage.relativePath}
+            />
+        )
+    } else {
+        return (
+            <PostItem
+                slug={hit.fields.slug}
+                title={hit.title}
+                date={hit.date}
+                description={hit.description}
+                category={hit.category}
+                thumbnailImage={hit.thumbnailImage.relativePath}
+                timeToRead={hit.timeToRead}
+            />
+        )
+    }
 }
 
 export default Hit;
