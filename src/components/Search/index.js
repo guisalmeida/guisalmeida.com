7f689c548b213af1d8e685450a31e5a1c4c83f91
guisalmeida <guilherme.dasilva@linx3.com>
2021-06-19 14:26:05 -0300

    feat: update algolia

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -6,28 +6,26 @@ import Hit from './Hit';
 
 import * as S from './styled';
 
-const algolia = {
-    appId: process.env.GATSBY_ALGOLIA_APP_ID,
-    searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
+const Search = ({ algolia }) => {
+    const searchClient = algoliasearch(
+        algolia.appId,
+        algolia.searchOnlyApiKey
+    )
+
+    return (
+        <S.SearchWrapper>
+            <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+                <SearchBox translations={{ placeholder: "Pesquisar..." }} />
+                <Stats translations={{
+                    stats (nbHits) {
+                        return `${nbHits} resultados encontrados.`
+                    }
+                }} />
+                <Hits hitComponent={Hit} />
+            </InstantSearch>
+        </S.SearchWrapper>
+    )
 }
-
-const searchClient = algoliasearch(
-    algolia.appId,
-    algolia.searchOnlyApiKey
-);
-
-const Search = () => (
-    <S.SearchWrapper>
-        <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
-            <SearchBox translations={{placeholder: "Pesquisar..."}}/>
-            <Stats translations={{stats(nbHits) {
-                return `${nbHits} resultados encontrados.`
-            }}} />
-            <Hits hitComponent={Hit}/>
-        </InstantSearch>
-    </S.SearchWrapper>
-)
 
 
 export default Search;
