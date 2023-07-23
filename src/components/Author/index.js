b86e0cd9f9e5bcc6df1fe5fd9c22ece0afce3912
“guisalmeida” <guisalmeida.dev@gmail.com>
2023-07-23 20:43:09 +0100

    refactor: change description

--- src/components/Author/index.js
+++ src/components/Author/index.js
@@ -8,7 +8,7 @@ const Author = ({ fullText }) => {
         <Base.MainContent>
             {fullText && 
             <h1>About</h1> || <h1>Hello!</h1>}
-            <p>I'm a Software Engineer focusing on Web Development, graduated with a Bachelor's of Computer Science (2022), and +4 years of experience in software development.
+            <p>I'm a Software Engineer focusing on Web Development, graduated with a Bachelor's of Computer Science with 4+ years of experience in software development.
             I worked on several projects, primarily in web development and mainly using Javascript, nodeJS, and Python. Responsible for creating very understandable and functional code. </p>
             <p>I'm passionate about learning new technologies and challenging jobs, sharing knowledge, and teamwork.</p>
             <p>Outside of work, I have a passion for skateboarding, and also I like cooking vegetarian recipes and spending time with my wife and dogs.</p>
@@ -21,7 +21,7 @@ const Author = ({ fullText }) => {
 
                 <ul>
                     <li>Javascript (Typescript / ES6+ / NodeJS / Jquery)</li>
-                    <li>Python</li>
+                    <li>Python (Flask / Pytest)</li>
                     <li>Java</li>
                     <li>Frameworks (ReactJS / VueJS)</li>
                     <li>HTML5 (SEO / Accessibility)</li>
