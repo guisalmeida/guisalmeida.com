77ea165cc548406af2536b96f3a0f1e92e4d8b6b
guisalmeida <guisalmeida.dev@gmail.com>
2023-10-06 14:52:34 -0300

    refactor: graphql query

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -1,32 +1,28 @@
-const postsQuery = `
-    {
-        posts: allMarkdownRemark(
-            sort: {fields: frontmatter___date, order: DESC }
-        ) {
-            edges {
-                node {
-                    objectID: id
-                    fields {
-                        slug
-                    }
-                    frontmatter {
-                        category
-                        date_timestamp: date
-                        date(locale: "en-us", formatString: "MMMM DD[,] YYYY")
-                        description
-                        title
-                        tags
-                        thumbnailImage {
-                            relativePath
-                        }
-                    }
-                    excerpt(pruneLength: 5000)
-                    timeToRead
-                }
-            }
+const postsQuery = `{
+  posts: allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
         }
+        frontmatter {
+          category
+          date_timestamp: date
+          date(locale: "en-us", formatString: "MMMM DD[,] YYYY")
+          description
+          title
+          tags
+          thumbnailImage {
+            relativePath
+          }
+        }
+        excerpt(pruneLength: 5000)
+        timeToRead
+      }
     }
-`
+  }
+}`
 
 const flatten = arr =>
     arr.map(({ node: { frontmatter, ...rest } }) => ({
